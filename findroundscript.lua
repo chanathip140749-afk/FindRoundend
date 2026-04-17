d35f5fa5ca2695f316e2ddd5f8c6f283807da4ac
chanathip140749-afk <chanathip140749@gmail.com>
2026-04-17 21:38:24 +0700

    Update findroundscript.lua

--- findroundscript.lua
+++ findroundscript.lua
@@ -3,17 +3,20 @@ local playerGui = player:WaitForChild("PlayerGui")
 local roundEnd = playerGui:WaitForChild("RoundEnd")
 local voteRemote = game:GetService("ReplicatedStorage"):WaitForChild("Systems"):WaitForChild("Voting"):WaitForChild("Vote")
 
--- ฟังก์ชันสำหรับส่งคำสั่ง Retry
 local function sendRetry()
     local args = {"Retry"}
     voteRemote:FireServer(unpack(args))
 end
 
--- ดักจับการเปลี่ยนแปลงของค่า Enabled ใน RoundEnd UI
 roundEnd:GetPropertyChangedSignal("Enabled"):Connect(function()
     if roundEnd.Enabled == true then
-        -- แนะนำให้รอสัก 1-2 วินาทีเพื่อให้แน่ใจว่าระบบบันทึก Reward เรียบร้อย
-        task.wait(0.5) 
-        sendRetry()
+        
+        task.wait(1) 
+        
+  
+        while roundEnd.Enabled == true do
+            sendRetry()
+            task.wait(1) 
+        end
     end
 end)
